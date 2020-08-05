df9ca7f2a266b72b5074a0b25a2dc4c96b636073
TiemaCoulibaly <47168830+TiemaCoulibaly@users.noreply.github.com>
2020-08-05 20:16:36 +0200

    link

--- app.js
+++ app.js
@@ -6,17 +6,53 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 app.set("view engine", "ejs");
 
+let posts = [];
+
 app.get("/", function (req, res) {
-	res.render("home");
+  res.render("home", {
+    posts: posts
+  });
+});
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
+
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.get("/posts/:post", function (req, res) {
+  const url = req.params.post;
+
+
+  posts.forEach(function (post) {
+    if (url === post.titleCompose) {
+      console.log("match found!!");
+    }
+  });
+});
+
+app.post("/compose", function (req, res) {
+  const composeContenu = {
+    titleCompose: req.body.postTitle,
+    contentCompose: req.body.postContent
+  };
+  posts.push(composeContenu);
+  res.redirect("/");
+
 });
 
 app.listen(3000, function () {
-	console.log("Server started!!");
+  console.log("Server started!!");
 });
