6548dbecbb9ad9c254c2602e99f64e228c03e5ba
TiemaCoulibaly <47168830+TiemaCoulibaly@users.noreply.github.com>
2020-08-05 20:44:22 +0200

    adding lodash

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 );
 
 app.set("view engine", "ejs");
@@ -16,43 +17,44 @@ app.set("view engine", "ejs");
 let posts = [];
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    posts: posts
-  });
+	res.render("home", {
+		posts: posts,
+	});
 });
 app.get("/about", function (req, res) {
-  res.render("about");
+	res.render("about");
 });
 app.get("/contact", function (req, res) {
-  res.render("contact");
+	res.render("contact");
 });
 
-
 app.get("/compose", function (req, res) {
-  res.render("compose");
+	res.render("compose");
 });
 
 app.get("/posts/:post", function (req, res) {
-  const url = req.params.post;
+	const url = req.params.post;
 
-
-  posts.forEach(function (post) {
-    if (url === post.titleCompose) {
-      console.log("match found!!");
-    }
-  });
+	posts.forEach(function (post) {
+		const titre = post.titleCompose;
+		if (_.lowerCase(url) === _.lowerCase(titre)) {
+			res.render("post", {
+				titlePost: post.titleCompose,
+				contentPost: post.contentCompose,
+			});
+		}
+	});
 });
 
 app.post("/compose", function (req, res) {
-  const composeContenu = {
-    titleCompose: req.body.postTitle,
-    contentCompose: req.body.postContent
-  };
-  posts.push(composeContenu);
-  res.redirect("/");
-
+	const composeContenu = {
+		titleCompose: req.body.postTitle,
+		contentCompose: req.body.postContent,
+	};
+	posts.push(composeContenu);
+	res.redirect("/");
 });
 
 app.listen(3000, function () {
-  console.log("Server started!!");
+	console.log("Server started!!");
 });
